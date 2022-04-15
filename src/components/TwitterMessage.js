430d5cecfaa6f96b20567e292f2e997642dd649e
“Daysha <+>
2022-04-15 08:43:57 -0700

    commit tree 0a19329b9535348e7bcbb39c8904796e27a4d7c4

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,17 +1,30 @@
 import React from "react";
 
 class TwitterMessage extends React.Component {
-  constructor() {
+  constructor(props) {
     super();
 
-    this.state = {};
+    this.state = {
+      totalChars: props.maxChars
+    };
+  }
+  
+  handleChange = (event) => {
+    const e = event.target
+    this.setState(prevState => {
+      return {
+        message: e.value,
+        totalChars: prevState.totalChars - 1
+      }
+    })  
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" onChange={this.handleChange} value={this.state.message} />
+        <p>Remaining characters: {this.state.totalChars}</p>
       </div>
     );
   }
